f55e51aace6f0290c34b8995b8b0db4032db5f59
chavraqul <raquel.chavez@hpe.com>
2015-10-27 19:40:41 -0500

    my stackoverflow assignent unit 4 lesson 2

---
+++ app.js
@@ -0,0 +1,186 @@
+$(document).ready( function() {
+	$('.unanswered-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getUnanswered(tags);
+	});
+	
+	$('.inspiration-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tag the user submitted
+		var tag = $(this).find("input[name='answerers']").val();
+		getTopAnswerers(tag);
+	});
+});
+
+// this function takes the question object returned by StackOverflow 
+// and creates new result to be appended to DOM
+var showQuestion = function(question) {
+	
+	// clone our result template code
+	var result = $('.templates .question').clone();
+	
+	// Set the question properties in result
+	var questionElem = result.find('.question-text a');
+	questionElem.attr('href', question.link);
+	questionElem.text(question.title);
+
+	// set the date asked property in result
+	var asked = result.find('.asked-date');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the #views for question property in result
+	var viewed = result.find('.viewed');
+	viewed.text(question.view_count);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+													question.owner.display_name +
+												'</a>' +
+							'</p>' +
+ 							'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	return result;
+};
+
+// This function takes an anwerer object returned by StackOverflow
+// and creates a new result to be appended to the DOM
+var showAnswerer = function (answerer) {
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+
+	// set the answerer name link
+	var inspirator = result.find('.answerer-name');
+	inspirator.html('<a target="_blank" href=http://stackoverflow.com/users/' + answerer.owner.user_id + ' >' +
+						answerer.owner.display_name + '</a>'
+	);
+
+	// set the reputaion points of the answerer
+	var reputationPts = result.find('.answerer-reputation');
+	reputationPts.text(answerer.owner.reputation);
+
+	// set the answerer accept rate
+	var acceptRate = result.find('.answerer-accept-rate');
+	acceptRate.text(answerer.owner.accept_rate);
+
+	// set the answerer post counts
+	var postCount = result.find('.answerer-posts');
+	postCount.text(answerer.owner.post_count);
+
+	// set the answerer scoring
+	var scoring = result.find('.answerer-score');
+	scoring.text(answerer.owner.score);
+
+	return result;
+}
+
+
+// this function takes the results object from StackOverflow
+// and creates info about search results to be appended to DOM
+var showSearchResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query;
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getUnanswered = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {tagged: tags,
+				   site: 'stackoverflow',
+				   order: 'desc',
+				   sort: 'creation'};
+	
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		data: request,
+		dataType: "jsonp",
+		type: "GET"
+		})
+	.done(function(result){
+		console.log(result);
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+// This function gets the top answerers for the tag passed as parameter.
+var getTopAnswerers = function (tagName) {
+	
+	// request parameters to pass to StackOverflow's API
+	var request = {tag: tagName,
+		           site: 'stackoverflow',
+				   period: 'all_time'
+				   };
+	var result = $.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/top-answerers",
+		data: request,
+		dataType: "jsonp",
+		type: "GET"
+		})
+	.done(function(result){
+		console.log(result);
+		var searchResults = showSearchResults(request.tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item) {
+			var anwerer = showAnswerer(item);
+			$('.results').append(anwerer);
+		});
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+/* Notes
+{
+  "items": [],
+  "has_more": false,
+  "quota_max": 10000,
+  "quota_remaining": 9982
+}
+
+Endpoint ~ /2.2/tags/{tag}/top-answerers/all_time?site=stackoverflow
+
+{
+  "user": {
+    "reputation": 9001,
+    "user_id": 1,
+    "user_type": "registered",
+    "accept_rate": 55,
+    "profile_image": "https://www.gravatar.com/avatar/a007be5a61f6aa8f3e85ae2fc18dd66e?d=identicon&r=PG",
+    "display_name": "Example User",
+    "link": "http://example.stackexchange.com/users/1/example-user"
+  },
+  "post_count": 100,
+  "score": 20
+}
+*/
+
